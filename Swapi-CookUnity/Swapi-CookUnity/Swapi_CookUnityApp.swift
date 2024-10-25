5047a7bacfdca9187bff1ca7cc9a946b8dc1e4da
Fernando Putallaz <ferhedio@gmail.com>
2024-10-25 18:01:24 -0300

    Creates Blank iOS Project

---
+++ Swapi-CookUnity/Swapi-CookUnity/Swapi_CookUnityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swapi_CookUnityApp.swift
+//  Swapi-CookUnity
+//
+//  Created by Fernando Putallaz on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swapi_CookUnityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
