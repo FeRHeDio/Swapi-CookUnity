4c84941adb76e8b9cc8018a6a814d082f14dd92f
Fernando Putallaz <ferhedio@gmail.com>
2024-10-29 08:58:48 -0300

    Move `ApiLoader` to it's own file

---
+++ Swapi-CookUnity/Swapi-CookUnity/Services/ApiLoader.swift
@@ -0,0 +1,49 @@
+//
+//  ApiLoader.swift
+//  Swapi-CookUnity
+//
+//  Created by Fernando Putallaz on 29/10/2024.
+//
+
+import Foundation
+
+class ApiLoader: PeopleLoaderProtocol {
+    func getPeople() async throws -> [People] {
+        do {
+            guard let url = URL(string: url) else {
+                throw URLError(.badURL)
+            }
+            
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw URLError(.badServerResponse)
+            }
+            
+            guard httpResponse.statusCode == 200 else {
+                throw URLError(.badServerResponse)
+            }
+            
+            let peopleResponse = try JSONDecoder().decode(PeopleResponse.self, from: data)
+            
+            if let nextPage = peopleResponse.next {
+                self.url = nextPage
+                print(nextPage)
+            } else {
+                hasMorePages = false
+            }
+            
+            return peopleResponse.results
+            
+        } catch {
+            throw URLError(.unknown)
+        }
+    }
+    
+    var url = "https://swapi.dev/api/people/?page=1"
+    var hasMorePages = true
+    
+    func resetCollection() {
+        self.url = "https://swapi.dev/api/people/?page=1"
+    }
+}
